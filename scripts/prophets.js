e9f832ae2bc0c700a2801efe892aa7c0edc30569
Thia100 <thegbolahanfathia@gmail.com>
2025-03-21 19:18:38 +0100

    chamber of commerce first commit

---
+++ scripts/prophets.js
@@ -0,0 +1,38 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+async function getProphetData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+getProphetData(url)
+
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let dateOfBirth = document.createElement('p');
+        let placeOfBirth = document.createElement('p');
+        let portrait = document.createElement('img');
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `portrait of ${prophet.name} ${prophet.lastName}`);
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+        dateOfBirth.textContent = `Date of Birth: ${prophet.birthdate}`;
+        placeOfBirth.textContent = `Place of Birth: ${prophet.birthplace}`;
+
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+        card.appendChild(dateOfBirth);
+        card.appendChild(placeOfBirth);
+
+        cards.appendChild(card);
+
+  
+    });
+}
