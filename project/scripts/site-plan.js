3904f9ebc46295b365ca7ea7a5473a2a1405237e
Thia100 <thegbolahanfathia@gmail.com>
2025-04-10 18:20:54 +0100

    project

--- project/scripts/site-plan.js
+++ project/scripts/site-plan.js
@@ -6,3 +6,17 @@ hamburgerElement.addEventListener('click', () =>{
     navElement.classList.toggle('open');
     hamburgerElement.classList.toggle('open');
 })
+
+const bookNow = document.querySelector('#nav-button');
+bookNow.addEventListener('click', () =>{
+    window.open('/final-project/booking.html', '_blank');
+});
+
+
+
+const faqs = document.querySelectorAll(".faq");
+faqs.forEach(faq => {
+  faq.addEventListener("click", () =>{
+    faq.classList.toggle("active");
+  })
+});
