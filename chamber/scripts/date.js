bead5436e269f696500fef16b1a4dd140276a9b2
Thia100 <thegbolahanfathia@gmail.com>
2026-06-01 00:03:49 +0100

    added timestamp

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -2,8 +2,10 @@ let copyrightYear = document.querySelector("#copyright-year");
 const year = new Date().getFullYear();
 copyrightYear.innerHTML = `&copy; ${year}`
 
-
-
 let lastModified = document.querySelector("#last-modified");
 let lastModifiedDate = new Date(document.lastModified).toLocaleString('en-US', {day: "numeric", month: "short", year: "numeric", hour: "2-digit", minute: "2-digit", second: "2-digit"});
 lastModified.innerHTML = `Last Modification: ${lastModifiedDate}`
+
+const timestamp = document.querySelector("#timestamp");
+
+timestamp.value = new Date().toISOString();
