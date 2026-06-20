9465f23c69fde05c57e6e4af6cf91317fac96d67
Thia100 <thegbolahanfathia@gmail.com>
2026-06-20 05:24:09 +0100

    index nav done

--- finalProject/scripts/navigation.js
+++ finalProject/scripts/navigation.js
@@ -1,9 +1,8 @@
-const navMenu = document.querySelector("#navMenu")
+const navMenu = document.querySelector(".navMenu")
 const openNav = document.querySelector(".open");
 openNav.addEventListener("click", () => {
     navMenu.classList.add("active")
 })
-
 
 const closeNav = document.querySelector(".close");
 closeNav.addEventListener("click", () => {
